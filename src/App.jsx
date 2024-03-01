90a4fb377eb9a12db3c91ffa7af2b6546b796fd9
israt Jahan Naishe <israt.ict.cou@gmail.com>
2024-03-01 11:06:57 +0200

    the function simply returns the result of the single expression without curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ const HelloWorld = () => {
 }
 
 const Hello = (props) => {
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    return yearNow - props.age
-  }
+  // const bornYear = () => {
+  //   const yearNow = new Date().getFullYear()
+  //   return yearNow - props.age
+  // }
+  const name = props.name
+  const age = props.age
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <section>
       <p>Hello {props.name}, you are {props.age} years old</p>
